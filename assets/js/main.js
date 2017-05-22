ff89494ddadf7eed3b2c44bf804f861319122036
Pola <Pola.kinomoto@gmail.com>
2017-05-22 00:22:04 -0400

    avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,6 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
-
+ var content= document.getElementById("01");
+ var img= document.createElement("img");
+ img.setAttribute("src", pictures[0]);
+ img.setAttribute("class", "imagen");
+ content.appendChild(img);
